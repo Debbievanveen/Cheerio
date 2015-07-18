f1aa3560c12ba46607ac6503dd99f9e5025350ef
Debbievv3 <van.veen.debbie@gmail.com>
2015-07-18 14:46:48 +0000

    frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   resources :surveys
   resources :answers
   resources :questions
+
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
